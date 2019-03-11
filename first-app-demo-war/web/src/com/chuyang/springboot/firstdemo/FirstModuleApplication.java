ab741a9f35993e9b8bf81bfe046c8979eacae4c2
chuyang <chuyangtj@gmail.com>
2019-03-11 16:54:17 +0800

    SpringBoot工程打包成war配置修改

--- first-app-demo-war/web/src/com/chuyang/springboot/firstdemo/FirstModuleApplication.java
+++ first-app-demo-war/web/src/com/chuyang/springboot/firstdemo/FirstModuleApplication.java
@@ -2,9 +2,18 @@ package com.chuyang.springboot.firstdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstModuleApplication {
+public class FirstModuleApplication extends SpringBootServletInitializer {
+
+
+	//重写SpringBootServletInitializer的configure方法
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FirstModuleApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstModuleApplication.class, args);
